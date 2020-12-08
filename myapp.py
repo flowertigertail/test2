f26c9f11330cb596b2bffbc1db481a1073285b29
flowertigertail <72187751+flowertigertail@users.noreply.github.com>
2020-12-09 02:43:55 +0900

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,11 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, url_for, request
 app = Flask(__name__)
 
-
-@app.route("/")
-def home():
-    return render_template('home.html')
-
+@app.route('/change', methods = ['POST'])
+def change():
+    return redirect(request.form['url'])
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
